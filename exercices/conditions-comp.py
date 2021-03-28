a5a754824d3dc9aec33f14cefc07882217df6545
Yuri Souza Santos <yurisouza1712@gmail.com>
2021-03-28 10:48:49 -0300

    rename the files and folder

---
+++ exercices/conditions-comp.py
@@ -0,0 +1,6 @@
+idade = int(input('Digite a idade de seu carro: '))
+if idade <= 3:
+    print('Seu carro é novo')
+# caso contrario (condicao complementar)
+else:
+    print('Seu carro é velho')
---
+++ exercices/print-f.py
@@ -0,0 +1,5 @@
+resposta = 42
+print(f'Tudo tem {resposta} como solução')
+
+preco = 9.6666
+print(f'Preço é R$ {preco: .2f}')
